d6f386c2081b9b6266c56e00be206e93212d82af
MukhammadSultan-Jurabekov <jurabekovmukhammadsultan@gmail.com>
2024-10-22 16:28:07 +0100

    Imports

---
+++ app.py
@@ -0,0 +1,6 @@
+import torch
+import torch.nn as nn
+import torchvision.transforms as transforms
+from torchvision.datasets import ImageFolder
+from torch.utils.data import DataLoader
+import torchvision.utils as vutils
