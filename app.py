1b1df296efa0a102b3d3dbe2c251649b531b316b
MukhammadSultan-Jurabekov <jurabekovmukhammadsultan@gmail.com>
2024-10-22 16:29:55 +0100

    Network parameters
    nz = 100  # Input Size
    ngf = 64  # The quantity of filters in generator
    ndf = 64  #The quantity of filters in discriminator
    nc = 3    # RGB

--- app.py
+++ app.py
@@ -4,3 +4,11 @@ import torchvision.transforms as transforms
 from torchvision.datasets import ImageFolder
 from torch.utils.data import DataLoader
 import torchvision.utils as vutils
+
+image_size = 64
+batch_size = 128
+nz = 100  # Размер входного вектора шума
+ngf = 64  # Количество фильтров в генераторе
+ndf = 64  # Количество фильтров в дискриминаторе
+nc = 3    # Количество цветовых каналов (RGB)
+
